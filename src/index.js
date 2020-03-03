dfecdb1270c19e4912764efd269023e3dc448699
K <ktoornlaaldlaiepxssi@gmail.com>
2020-03-03 18:56:58 +0300

    horizontal scroll fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import "./scss/main.scss";
 require("regenerator-runtime/runtime");
+// var ghpages = require("gh-pages");
 
+// ghpages.publish("dist", function(err) {});
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const statsUrl =
   "https://www.codewars.com/api/v1/users/Laexssi/code-challenges/completed?page=0";
 
 const kataStats = document.querySelector(".codewars__stats > span");
 async function fetchData(url) {
   const response = await fetch(url);
+  console.log(response);
   const data = await response.json();
   console.log(data);
   kataStats.textContent = `Completed Kata: ${data.totalItems}`;
 }
 
-document.addEventListener("DOMContentLoaded", () => fetchData(statsUrl));
+document.addEventListener("DOMContentLoaded", () =>
+  fetchData(proxyUrl + statsUrl)
+);
 
 console.log(kataStats);
